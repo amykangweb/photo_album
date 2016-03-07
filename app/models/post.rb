a8acf6bc6e90196e57c55258a64e755fc34f2abf
amykangweb <selene6023@gmail.com>
2016-03-06 19:40:10 -0800

    Set root to posts index. Remove show file and show action in posts controller.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
+  mount_uploader :image, ImageUploader
   validates :image, presence: true
   validates :caption, presence: true, length: { maximum: 100 }
 end
