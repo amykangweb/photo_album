0a932c4b59f8d24b4fa42b5f50ddd8301bdfb6fe
amykangweb <selene6023@gmail.com>
2016-03-06 20:28:13 -0800

    Generate migration for uid and provider for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
   resources :users
   resources :posts
+  resources :admin
 
   root 'posts#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307035718) do
+ActiveRecord::Schema.define(version: 20160307042441) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "image"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20160307035718) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "uid"
+    t.string   "provider"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
