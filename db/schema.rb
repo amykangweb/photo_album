345aa0795f2fd1543cf39139493ccdac4572c599
amykangweb <selene6023@gmail.com>
2016-03-06 19:54:43 -0800

    Scaffold Users resource.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307031351) do
+ActiveRecord::Schema.define(version: 20160307035416) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "image"
     t.string   "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
