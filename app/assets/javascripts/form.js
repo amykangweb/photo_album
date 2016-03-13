6dd4fcaa18d8d9b57cc1df3667ceaa20393ddc7e
amykangweb <selene6023@gmail.com>
2016-03-12 21:24:22 -0800

    Can hide and show admin and visitor menu.

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   $('.new-post').click(function(){
     $('.hidden-form').show();
+    $('.hidden-menu').hide();
   });
   $('.hidden-form-bg').click(function(){
     $('.hidden-form').hide();
+    $('.admin-menu').show();
   });
 });
